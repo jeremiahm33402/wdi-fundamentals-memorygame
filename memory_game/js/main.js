07ba759e5ce523d59ea5d866fe59bf4f069dd1cc
jeremiahm33402 <wjmiller@up.com>
2020-03-29 21:43:16 -0500

    Objects, Objects, & More Objectsgit add -A

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png",	
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png",
+},
+];
 const cardsInPlay = [];
 let cardId;
 
@@ -14,12 +35,14 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 
-	console.log("User flipped " +cards[cardId]);
+	console.log("User flipped " +cards[cardId].rank);
 
-	cardsInPlay.push(cards[0]);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	
 }
 
